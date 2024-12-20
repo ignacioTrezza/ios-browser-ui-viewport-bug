81dade36de6dac76a0b9b339a5db0e150956499b
ignacioTrezza <ignacio.trezza@gmail.com>
2024-12-20 17:45:25 -0300

    first try ios viewport bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Page1Component,
+    Page2Component,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
+// Adjust --vh custom property on window resize
+function setVh() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+// Initial setting
+setVh();
+
+// Update on resize
+window.addEventListener('resize', setVh);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
